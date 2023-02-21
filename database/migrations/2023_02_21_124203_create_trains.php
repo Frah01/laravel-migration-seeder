1f079516a36d36b40144cec39b264823e9ab5a12
Frah01 <vinario.francesco@gmail.com>
2023-02-21 14:00:11 +0100

    Creato il migrate

--- database/migrations/2023_02_21_124203_create_trains.php
+++ database/migrations/2023_02_21_124203_create_trains.php
@@ -16,6 +16,15 @@ return new class extends Migration
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('Azienda');
+            $table->string('Stazione_di_Partenza');
+            $table->string('Stazione_di_Arrivo');
+            $table->string('Orario_di_Partenza');
+            $table->string('Orario_di_Arrivo');
+            $table->smallInteger('Codice_Treno');
+            $table->smallInteger('Numero_Carrozze');
+            $table->boolean('In_Orario');
+            $table->boolean('Cancellato');
         });
     }
 
